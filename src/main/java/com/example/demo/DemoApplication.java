45f8965cf7eb797b23f39fe5718c12023b8759c2
PurnaSesham <purnasesham4@gmail.com>
2023-10-20 11:26:23 +0530

    test
    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	//SpringApplication is bootstrap the application
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
